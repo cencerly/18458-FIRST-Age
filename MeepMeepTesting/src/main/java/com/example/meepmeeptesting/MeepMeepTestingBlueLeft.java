440ee7e1febe80d3b7bcf2581c108b13b3e33fae
victo <victorsm1022@gmail.com>
2025-10-24 12:55:10 -0500

    what leo wanted me to do

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTestingBlueLeft.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTestingBlueLeft.java
@@ -1,14 +1,13 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.TrajectoryBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-public class MeepMeepTestingBlue {
+public class MeepMeepTestingBlueLeft {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(600);
 
@@ -22,10 +21,7 @@ public class MeepMeepTestingBlue {
 
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(63, 10, Math.toRadians(180)))
 
-                .splineTo(new Vector2d(36, 45), Math.toRadians(90))
 
-                        . strafeToConstantHeading(new Vector2d(0, 20))
-                        .splineTo(new Vector2d(-40, 30), Math.toRadians(120))
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_DARK)
@@ -34,5 +30,4 @@ public class MeepMeepTestingBlue {
                 .addEntity(myBot)
                 .start();
     }
-    //goon
 }
---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTestingRedLeft.java
@@ -0,0 +1,31 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class MeepMeepTestingRedLeft {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(600);
+
+        RoadRunnerBotEntity myFirstBot = new DefaultBotBuilder(meepMeep)
+                .setColorScheme(new ColorSchemeRedDark())
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        myFirstBot.runAction(myFirstBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
+
+
+
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myFirstBot)
+                .start();
+    }
+
+}
