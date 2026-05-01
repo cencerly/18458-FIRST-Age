72cc1eab22434aae0f21a5f85bbaaeaae52a2c61
victo <victorsm1022@gmail.com>
2026-05-01 12:05:24 -0500

    small auto thingy idk

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleAutoPathing.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.pedropathing.util.Timer;
+
+@Autonomous
+public class SampleAutoPathing extends OpMode{
+
+    private Follower follower;
+    private Timer pathTimer, opModeTime;
+
+    public enum PathState{
+        //start pos_endpos
+        // drive > movestate
+        // shoot> try to score
+
+    }
+
+    PathState pathState;
+
+    private final Pose startPose = new Pose(110.7913043478261,135.00000000000003, Math.toRadians(90));
+    private final Pose shootPose = new Pose(101.44347826086955, 99.78260869565216, Math.toRadians(90));
+
+    private PathChain
+
+    @Override
+    public void init (){
+
+    }
+
+    @Override
+    public void loop(){
+
+    }
+}
