af8c3fa73d106de6cb0ae733cca1b9893a046700
jungojun03 <damianjungo10@gmail.com>
2026-05-01 17:11:14 -0500

    lalala

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/BlueAuto15.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.Autos;
+
+import com.pedropathing.follower.Follower;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.pedropathing.util.Timer;
+
+@TeleOp
+public class BlueAuto15 extends OpMode {
+    private Follower follower;
+    private Timer pathTimer, opModeTimer;
+
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
